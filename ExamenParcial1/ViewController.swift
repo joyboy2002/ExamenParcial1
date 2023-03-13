8e020e0943dae0b909c16b2c0b22dbe5e4a90d3b
Emiliano Lopez <lopezemiliano195@gmail.com>
2023-03-13 17:06:24 -0600

    Initial Commit

---
+++ ExamenParcial1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenParcial1
+//
+//  Created by ISSC_611_2023 on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
